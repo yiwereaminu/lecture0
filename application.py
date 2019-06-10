c3a7b240a48874f1fdf15c4695765eb215d9c81a
Aminu Yiwere <yiwereamin@gmail.com>
2019-06-10 19:22:37 +0000

    editted application.py

--- application.py
+++ application.py
@@ -4,3 +4,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "hello world"
+
+@app.route("/amin")
+def amin():
+    return "Hello, Amin"
+
+@app.route("/yiwere")
+def yiwere():
+    return "Hello, Yiwere"
+
+//dynamic route
+@app.route("/<string:name>")
+def dynamic(name):
+    return f"Hello, {name}"
