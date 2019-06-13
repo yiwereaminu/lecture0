ba25052bc3a3f3baa95210079aed85eb7bebb002
Aminu Yiwere <yiwereamin@gmail.com>
2019-06-13 17:22:09 +0000

    a whole bunch of things

--- application.py
+++ application.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("layout.html")
 
-@app.route("/amin")
-def amin():
-    return "Hello, Amin"
+#@app.route("/bye")
+#def bye():
+#    headline = "Good bye!"
+#    return render_template("index.html", headline=headline)
 
-@app.route("/yiwere")
-def yiwere():
-    return "Hello, Yiwere"
+#@app.route("/callUsNow")
+#def callUsNow():
+#    callUsNow = "0558263462"
+#    return render_template("index.html", callUsNow=callUsNow)
 
-
-@app.route("/<string:name>")
-def dynamic(name):
-    return f"Hello, {name}"
+@app.route("/hello", methods = ["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html", name=name)
